14335893ef6aa9e93aaa1917be9057523dec8ab2
wahome <wahomewgitonga@gmail.com>
2024-05-22 10:33:51 +0300

    update

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    for i in range(10, 0, -1):
+        print(i)
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [i**2 for i in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 15 == 0: print("FizzBuzz")
+        elif i % 3 == 0: print("Fizz")
+        elif i % 5 == 0: print("Buzz")
+        else: print(i)
